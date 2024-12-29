d0a614e2ccd5fd7b1171c5a1e2ef1176f3423723
sturioo <shopify.igor10@gmail.com>
2024-12-29 15:45:58 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+    unoptimized: true
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+    return config;
+  },
+}
+
+module.exports = nextConfig
+
