efee26235ac7fc84f04737cfdde0ca1f48663294
sturioo <shopify.igor10@gmail.com>
2024-12-29 15:40:13 +0100

    Create _app.js

---
+++ _app.js
@@ -0,0 +1,12 @@
+import { LazyMotion, domAnimation } from 'framer-motion'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <LazyMotion features={domAnimation}>
+      <Component {...pageProps} />
+    </LazyMotion>
+  )
+}
+
+export default MyApp
+
