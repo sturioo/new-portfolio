303bf61254680448625b6468eeddaedf6a1556e1
sturioo <shopify.igor10@gmail.com>
2024-12-29 15:42:23 +0100

    Create kontakt.tsx

---
+++ kontakt.tsx
@@ -0,0 +1,8 @@
+import { Button } from "./button"
+import { Input } from "./input"
+import { Textarea } from "./textarea"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select"
+import { Label } from "./label"
+
+// rest of the code
+
