63b5184887428d6b19a5cc4a8ec2c892a35911d6
sturioo <shopify.igor10@gmail.com>
2024-12-29 15:44:49 +0100

    Create textarea.tsx

---
+++ textarea.tsx
@@ -0,0 +1,13 @@
+import { cn } from "./utils"
+
+// rest of the code
+function MyComponent() {
+  return (
+    <div className={cn("bg-white", "p-4")}>
+      {/* Component content */}
+    </div>
+  );
+}
+
+export default MyComponent;
+
