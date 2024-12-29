cc1a0131074a39bb3a4790cf553027f3ff37d1d6
sturioo <shopify.igor10@gmail.com>
2024-12-29 15:42:49 +0100

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,17 @@
+import Header from './Header'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        {/* Main content here */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
